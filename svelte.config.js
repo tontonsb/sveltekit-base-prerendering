33c6b0e6a22fb1a62c78c4da51fb6ab239d9b7fb
Džuris <juris@glaive.pro>
2023-01-16 23:42:14 +0200

    Switch adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*',
+				'/en',
+			],
+		},
+	},
 };
 
 export default config;
