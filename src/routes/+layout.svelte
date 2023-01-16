24c6ca0379cf7c7a8e8063ca90bd1fe334b1383d
Džuris <juris@glaive.pro>
2023-01-16 22:47:14 +0200

    Add pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {page} from '$app/stores'
+
+    $: lang = $page.params.lang ?? 'en'
+</script>
+
+<svelte:head>
+    <base href="/{lang}/">
+</svelte:head>
+
+<nav>
+    <a href="/{lang}">Index</a>
+    <a href="section">Section</a>
+    <a href="section/subsection">Subsection</a>
+</nav>
+
+<slot />
+
+<style>
+    nav {
+        display: flex;
+        gap: 1rem;
+    }
+</style>
